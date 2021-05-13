80f78b47e675174eb680aef45458a006b198a2e2
UseMuse <UseMuse@mail.ru>
2021-05-13 17:04:49 +0300

    Добавьте файлы проекта.

---
+++ ASP.NET5.MVC.WEBPACK/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET5.MVC.WEBPACK.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
