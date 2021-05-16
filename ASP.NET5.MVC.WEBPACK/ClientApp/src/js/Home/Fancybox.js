43d6af58c5665405b0ca47a899d75eb1d884f36a
UseMuse <UseMuse@mail.ru>
2021-05-16 17:31:53 +0300

    added example of use fancyapps fancybox

--- ASP.NET5.MVC.WEBPACK/ClientApp/src/js/Home/Fancybox.js
+++ ASP.NET5.MVC.WEBPACK/ClientApp/src/js/Home/Fancybox.js
@@ -1,1 +1,4 @@
-﻿console.log('Add js bundle manually in view. The \'Home Fancybox\' bundle has been loaded!');
+﻿import("@fancyapps/fancybox/dist/jquery.fancybox.min.css");
+import("@fancyapps/fancybox");
+
+console.log('Add js bundle manually in view. The \'Home Fancybox\' bundle has been loaded!');
