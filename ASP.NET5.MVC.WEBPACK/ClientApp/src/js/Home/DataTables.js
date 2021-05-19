f08cbfae4be69ef7c8850708f9a970795f0d28b0
UseMuse <UseMuse@mail.ru>
2021-05-19 16:17:58 +0300

    datatables fetch example

--- ASP.NET5.MVC.WEBPACK/ClientApp/src/js/Home/DataTables.js
+++ ASP.NET5.MVC.WEBPACK/ClientApp/src/js/Home/DataTables.js
@@ -8,8 +8,29 @@
 import 'datatables.net-bs/css/dataTables.bootstrap.css';
 import dt from 'datatables.net';
 import dt_bs from 'datatables.net-bs';
+import api from '../requestApi';
 //when add the file dynamically from 'router.js' this entry is not needed, it will work without it
 //uncomment if you want to manually include the file in view
 window.$.fn.DataTable = dt_bs
+
+export async function renderTable() {
+    /*    (async () => {*/
+    let data = await api.get('https://jsonplaceholder.typicode.com/users');
+    $('#example').DataTable({
+        data: data,
+        "columns": [
+            { "data": "name" },
+            { "data": "username" },
+            { "data": "email" },
+            { "data": "address.street" },
+            { "data": "company.name" },
+            { "data": "phone" },
+            { "data": "website" }
+        ]
+    });
+    /*    })();*/
+}
+
+
 
 console.log('Add js bundle manually in view. The \'Home DataTables\' bundle has been loaded!');
